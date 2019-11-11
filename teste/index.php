138a43d2fb0a2c03f5e1842288e9dd58c2fb4bdf
Vitor Emanuel <veslima3@gmail.com>
2019-11-11 15:40:25 -0300

    Update index.php

--- teste/index.php
+++ teste/index.php
@@ -2,16 +2,28 @@
 
 <h1>Bem vindo(a)!</h1>
 
-<?php
- if(have_posts()){
-        while(have_posts()){
-            the_post();
- ?>
+<main class="home-main">
+    <div class="container">
+
+        <ul class="imoveis-listagem">
+        <?php
+            if(have_posts()){
+            while(have_posts()){
+              the_post();
+        ?>
+            <li class="imoveis-listagem-item">
+            <?php the_post_thumbnail(); ?>
     <h2><?php the_title(); ?></h2>
     <div><?php the_content();?></div>  
-<?php       
-    }
-}
-    ?>
+            </li>
+
+        <?php       
+            }
+        }
+        ?>
+
+        </ul>
+    </div>
+</main>
 
 <?php get_footer();?>
