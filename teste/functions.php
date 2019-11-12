762718b680226473579a373f8da615de09788214
Vitor Emanuel <veslima3@gmail.com>
2019-11-12 13:01:40 -0300

    Update functions.php

--- teste/functions.php
+++ teste/functions.php
@@ -2,4 +2,37 @@
 
 add_theme_support('post-thumbnails');
 
+function cadastrando_post_type_imoveis(){
+
+    $nomeSingular = 'Imóvel';
+    $nomePlural = 'Imóveis';
+    $description = "Imobiliária Quarto Andar";
+    
+    $labels = array(
+        'name' => $nomePlural,
+        'name_singular' => $nomeSingular,
+        'add_new_item' => 'Adicionar novo ' . $nomeSingular,
+        'edit_item' => 'Editar ' . $nomeSingular
+    );
+
+    $supports = array(
+        'title',
+        'editor',
+        'thumbnail'
+    );
+    
+    $args = array(
+        'labels' => $labels,
+        'public' => true,
+        'description' => $description,
+        'menu_icon' => 'dashicons-admin-home',
+        'supports' => $supports
+    );
+    
+    register_post_type('imovel', $args); 
+
+}
+
+add_action('init', 'cadastrando_post_type_imoveis');
+
 ?>
