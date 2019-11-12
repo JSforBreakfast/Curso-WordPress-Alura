385de4c52840ad3da3f50f1e170d20b1ac1e838c
Vitor Emanuel <veslima3@gmail.com>
2019-11-12 10:43:05 -0300

    Update header.php

--- teste/header.php
+++ teste/header.php
@@ -1,7 +1,10 @@
 <!DOCTYPE html>
 <html>
 <head>
+    <?php $home = get_template_directory_uri(); ?>
     <meta charset="utf-8">
+    <link rel="stylesheet" type="text/css" href="<?php echo $home ?>/reset.css">
+    <link rel="stylesheet" type="text/css" href="<?php echo $home ?>/style.css">
     <?php wp_head();?>
 </head>
 
