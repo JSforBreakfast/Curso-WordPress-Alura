e196e955c3576c9f16d01dbb3cde2e9fbe5f3402
Vitor Emanuel <veslima3@gmail.com>
2019-11-12 15:03:19 -0300

    Update functions.php

--- teste/functions.php
+++ teste/functions.php
@@ -1,38 +1,37 @@
-<?php 
+<?php
 
-add_theme_support('post-thumbnails');
+add_theme_support( 'post-thumbnails' );
 
-function cadastrando_post_type_imoveis(){
+function registrar_imoveis() {
+	$descricao = 'Usado para listar os imóveis da Maluras';
+	$singular = 'Imóvel';
+	$plural = 'Imóveis';
 
-    $nomeSingular = 'Imóvel';
-    $nomePlural = 'Imóveis';
-    $description = "Imobiliária Quarto Andar";
-    
-    $labels = array(
-        'name' => $nomePlural,
-        'name_singular' => $nomeSingular,
-        'add_new_item' => 'Adicionar novo ' . $nomeSingular,
-        'edit_item' => 'Editar ' . $nomeSingular
-    );
+	$labels = array(
+		'name' => $plural,
+		'singular_name' => $singular,
+		'view_item' => 'Ver ' . $singular,
+		'edit_item' => 'Editar ' . $singular,
+		'new_item' => 'Novo ' . $singular,
+		'add_new_item' => 'Adicionar novo ' . $singular
+	);
 
-    $supports = array(
-        'title',
-        'editor',
-        'thumbnail'
-    );
-    
-    $args = array(
-        'labels' => $labels,
-        'public' => true,
-        'description' => $description,
-        'menu_icon' => 'dashicons-admin-home',
-        'supports' => $supports
-    );
-    
-    register_post_type('imovel', $args); 
+	$supports = array(
+		'title',
+		'editor',
+		'thumbnail'
+	);
 
+	$args = array(
+		'labels' => $labels,
+		'description' => $descricao,
+		'public' => true,
+		'menu_icon' => 'dashicons-admin-home',
+		'supports' => $supports
+	);
+
+
+	register_post_type( 'imovel', $args);	
 }
 
-add_action('init', 'cadastrando_post_type_imoveis');
-
-?>
+add_action('init', 'registrar_imoveis');
