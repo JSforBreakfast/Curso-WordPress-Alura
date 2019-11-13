57e5e20ea1fb17018d613555417c2420849eabde
Vitor Emanuel <veslima3@gmail.com>
2019-11-13 15:40:55 -0300

    Update header.php

--- teste/header.php
+++ teste/header.php
@@ -3,10 +3,30 @@
 <head>
     <?php $home = get_template_directory_uri(); ?>
     <meta charset="utf-8">
-    <link rel="stylesheet" type="text/css" href="<?php echo $home ?>/reset.css">
-    <link rel="stylesheet" type="text/css" href="<?php echo $home ?>/style.css">
+    <link rel="stylesheet" type="text/css" href="<?php echo $home ?>/assets/css/comum.css">
+    <link rel="stylesheet" type="text/css" href="<?php echo $home ?>/assets/css/header.css">
+    <link rel="stylesheet" type="text/css" href="<?php echo $home ?>/assets/css/<?php $css_especifico; ?>.css">
+    <link rel="stylesheet" type="text/css" href="<?php echo $home ?>/assets/css/<?php $css_especifico; ?>.css">
+    <link rel="stylesheet" type="text/css" href="<?php echo $home ?>/assets/css/reset.css">
+    <link rel="stylesheet" type="text/css" href="<?php echo $home ?>/assets/css/<?php $css_especifico; ?>.css">
+   
+    <title>
+    <?php geraTitle() ?>
+    </title>
+    
     <?php wp_head();?>
 </head>
 
 <body>
 
+<header>
+    <div class="container">
+        <?php 
+            $args = array(
+                'theme_location' => 'header-menu'
+            );
+            wp_nav_menu($args);
+        ?>
+    </div>
+
+</header>
