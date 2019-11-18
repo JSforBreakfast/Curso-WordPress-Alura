a4c09762c7b7b0c06fee8030474d7697cbf3d07a
Vitor Emanuel <veslima3@gmail.com>
2019-11-18 12:20:47 -0300

    Update index.php

--- teste/index.php
+++ teste/index.php
@@ -1,19 +1,38 @@
 <?php
+$queryTaxonomy = array_key_exists('taxonomy', $_GET);
+if($queryTaxonomy && $_GET['taxonomy'] === '') {
+    wp_redirect(home_url());
+}
 $css_escolhido = 'index';
 require_once('header.php');
 ?>
 <main class="home-main">
 	<div class="container">
 
+    <?php $taxonomias = get_terms('localizacao'); ?>
+    <form class="busca-localizacao-form" action="<?php bloginfo('url'); ?>">
+        <div name="taxonomy-select-wrapper">
+       <select class="taxonomy">
+       <option value="">Todos os imóveis</option>
+       <?php foreach($taxonomias as $taxonomia){ ?>
+            <option value="<?php echo $taxonomia->slug; ?>"><?php echo $taxonomia->name; ?>
+            </option>
+       <?php } ?>
+       </select>
+       </div>     
+       <button type="submit">Filtrar</button>
+    </form>
+
         <?php 
-        
-        $taxQuery = array(
-            array(
-            'taxonomy' =>'localizacao',
-            'field' => 'slug',
-            'terms' => 'salvador'
-            )
-        );
+        if($queryTaxonomy) {
+            $taxQuery = array(
+                array(
+                'taxonomy' =>'localizacao',
+                'field' => 'slug',
+                'terms' => $_GET['taxonomy'] //https://www.php.net/manual/en/reserved.variables.get.php
+                )
+            );
+        }
 
 			$args = array( 
                 'post_type' => 'imovel',
