50a88c7584d89804443909bc11d0335fc83b2340
Vitor Emanuel <veslima3@gmail.com>
2019-11-18 15:20:33 -0300

    Update functions.php

--- teste/functions.php
+++ teste/functions.php
@@ -3,7 +3,7 @@
 add_theme_support( 'post-thumbnails' );
 
 function registrar_imoveis() {
-	$descricao = 'Usado para listar os imóveis do Quarto Andar';
+	$descricao = 'Usado para listar os imóveis da Maluras';
 	$singular = 'Imóvel';
 	$plural = 'Imóveis';
 
@@ -31,62 +31,89 @@ function registrar_imoveis() {
 	);
 
 
-	register_post_type('imovel', $args);	
+	register_post_type( 'imovel', $args);	
 }
 
 add_action('init', 'registrar_imoveis');
 
-
+/* Registrar menu de navegação */
 
 function registrar_menu_navegacao() {
-    register_nav_menu('header-menu', 'main-menu');
+	register_nav_menu('header-menu', 'Menu Header');
 }
 
 add_action( 'init', 'registrar_menu_navegacao');
 
-function geraTitle() {
-	blog_info('name'); 
-	if(!is_home()) echo ' | ';
-	the_title();
- }
+function get_titulo() {
+	if( is_home() ) {
+		bloginfo('name');
+	} else {
+		bloginfo('name');
+		echo ' | ';
+		the_title();
+	}
+}
 
-function registrar_taxonomia_localizacao(){
+/* Criar a taxonomia de localização */
 
-	$nomeSingular = 'Localizacao';
-	$nomePlural = 'Localizacoes';
-	
+function criando_taxonomia_localizacao() {
+	$singular = 'Localização';
+	$plural = 'Localizações';
+
 	$labels = array(
-		'name' => $nomePlural,
-		'singular_name' => $nomeSingular,
-		'edit_item' => 'Editar' . $nomeSingular,
-		'add_new_item' => 'Adicionar nova ' . $nomeSingular
-	);
-	
-	 $args = array(
+		'name' => $plural,
+		'singular_name' => $singular,
+		'view_item' => 'Ver ' . $singular,
+		'edit_item' => 'Editar ' . $singular,
+		'new_item' => 'Novo ' . $singular,
+		'add_new_item' => 'Adicionar novo ' . $singular
+		);
+
+	$args = array(
 		'labels' => $labels,
 		'public' => true,
 		'hierarchical' => true
-	 );
-	
-	register_taxonomy('localizacao', 'imovel', $args);
+		);
 
+	register_taxonomy('localizacao', 'imovel', $args);
 }
 
-add_action('init', 'registrar_taxonomia_localizacao');
+add_action( 'init' , 'criando_taxonomia_localizacao' );
 
-function preenche_conteudo_informacoes_imovel(){?>
+function is_selected_taxonomy($taxonomy, $search) {
+	if($taxonomy->slug === $search) {
+		echo 'selected';
+	}
+}
+
+function adicionar_meta_info_imovel() {
+	add_meta_box(
+		'informacoes_imovel',
+		'Informações',
+		'informacoes_imovel_view',
+		'imovel',
+		'normal',
+		'high'
+	);
+}
+
+add_action('add_meta_boxes', 'adicionar_meta_info_imovel');
+
+function informacoes_imovel_view( $post ) { 
+	$imoveis_meta_data = get_post_meta( $post->ID ); ?>
+
 	<style>
-		.maluras-metabox {
+		.quarto-metabox {
 			display: flex;
 			justify-content: space-between;
 		}
 
-		.maluras-metabox-item {
+		.quarto-metabox-item {
 			flex-basis: 30%;
 
 		}
 
-		.maluras-metabox-item label {
+		.quarto-metabox-item label {
 			font-weight: 700;
 			display: block;
 			margin: .5rem 0;
@@ -113,7 +140,7 @@ function preenche_conteudo_informacoes_imovel(){?>
 			color: #FFF;
 		}
 
-		.maluras-metabox-input {
+		.quarto-metabox-input {
 			height: 100%;
 			border: 1px solid #CCC;
 			border-left: none;
@@ -126,40 +153,54 @@ function preenche_conteudo_informacoes_imovel(){?>
 			<label for="quarto-preco-input">Preço:</label>
 			<div class="input-addon-wrapper">
 				<span class="input-addon">R$</span>
-				<input id="quarto-preco-input" class="quarto-metabox-input" type="text" name="preco_id"
+				<input id="quarto-preco-input" class="maluras-metabox-input" type="text" name="preco_id"
 				value="<?= number_format($imoveis_meta_data['preco_id'][0], 2, ',', '.'); ?>">
 			</div>
 		</div>
 
 		<div class="quarto-metabox-item">
 			<label for="quarto-vagas-input">Vagas:</label>
-			<input id="quarto-vagas-input" class="quarto-metabox-input" type="number" name="vagas_id"
+			<input id="quarto-vagas-input" class="maluras-metabox-input" type="number" name="vagas_id"
 			value="<?= $imoveis_meta_data['vagas_id'][0]; ?>">
 		</div>
 
 		<div class="quarto-metabox-item">
 			<label for="quarto-banheiros-input">Banheiros:</label>
-			<input id="quarto-banheiros-input" class="quarto-metabox-input" type="number" name="banheiros_id"
+			<input id="quarto-banheiros-input" class="maluras-metabox-input" type="number" name="banheiros_id"
 			value="<?= $imoveis_meta_data['banheiros_id'][0]; ?>">
 		</div>
 
 		<div class="quarto-metabox-item">
 			<label for="quarto-quartos-input">Quartos:</label>
-			<input id="quarto-quartos-input" class="quarto-metabox-input" type="number" name="quartos_id"
-			value="<?php echo $imoveis_meta_data['quartos_id'][0]; ?>">
+			<input id="quarto-quartos-input" class="maluras-metabox-input" type="number" name="quartos_id"
+			value="<?= $imoveis_meta_data['quartos_id'][0]; ?>">
 		</div>
 
 	</div>
-<?php }
+<?php
 
-function registra_meta_boxes(){
-	add_meta_box('informacoes-imoveis',
-	'Informacoes do Imovel' ,
-	'preenche_conteudo_informacoes_imovel',
-	'imovel',
-	'normal',
-	'high'
-	);
 }
-add_action('add_meta_boxes', 'registra_meta_boxes');
-?>
+
+function salvar_meta_info_imoveis( $post_id ) {
+	if( isset($_POST['preco_id']) ) {
+		update_post_meta( $post_id, 'preco_id', sanitize_text_field( $_POST['preco_id'] ) );
+	}
+	if( isset($_POST['vagas_id']) ) {
+		update_post_meta( $post_id, 'vagas_id', sanitize_text_field( $_POST['vagas_id'] ) );
+	}
+	if( isset($_POST['banheiros_id']) ) {
+		update_post_meta( $post_id, 'banheiros_id', sanitize_text_field( $_POST['banheiros_id'] ) );
+	}
+	if( isset($_POST['quartos_id']) ) {
+		update_post_meta( $post_id, 'quartos_id', sanitize_text_field( $_POST['quartos_id'] ) );
+	}
+}
+
+add_action('save_post', 'salvar_meta_info_imoveis');
+
+function enviar_e_checar_email($nome, $email, $mensagem) {
+		return wp_mail( 'joanaquarto@gmail.com', 'Email Quarto', 'Nome: ' . $nome . "\n" . $mensagem  );
+}
+
+
+
